be37d401d48a50a9c48b752b942ca3e92457f65d
keiluv <21800510@handong.edu>
2020-12-10 21:57:52 +0900

    base

--- src/main/java/com/booksearch/booksearchapp/user/LoginController.java
+++ src/main/java/com/booksearch/booksearchapp/user/LoginController.java
@@ -8,8 +8,6 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-import com.booksearch.booksearchapp.user.UserDAO.UserServiceImpl;
-
 @Controller
 @RequestMapping(value="/login")
 public class LoginController {
